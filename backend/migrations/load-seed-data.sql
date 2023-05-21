030cec00f57a187f11c35269ae03f8784c7f4ef0
Nicolas Wormser <nicolas@codewithflair.org>
2023-05-21 23:36:05 +0200

    Add initial migrations

---
+++ backend/migrations/load-seed-data.sql
@@ -0,0 +1,66 @@
+INSERT INTO resources
+  (title, url, type, topics, description)
+  VALUES
+  (
+    "Toxicode's Silent Teacher",
+    "https://silentteacher.toxicode.fr/",
+    "Practice",
+    "Algorithms",
+    "Interface web ludique pour vous aider à comprendre quelques bases JS/Algo."
+  ),
+  (
+    "Scrimba's React free course",
+    "https://scrimba.com/learn/learnreact/",
+    "Course",
+    "React",
+    "Un cours bien construit (en anglais) avec de l'interactivité pour appliquer les concepts"
+  ),
+  (
+    "Exercism",
+    "https://exercism.org",
+    "Practice",
+    "Algorithms, Javascript",
+    "Des challenge JS avec une progression en difficulté, accès a des mentor pour review votre code gratuitement"
+  ),
+  (
+    "Codewars",
+    "https://codewars.com",
+    "Practice",
+    "Algorithms, Javascript",
+    "Plus de katas JS."
+  ),
+  (
+    "Can I Use?",
+    "https://caniuse.com/",
+    "Documentation",
+    "HTML, Javascript, CSS",
+    "Pour savoir si une fonctionnalité est gérée par les navigateurs"
+  ),
+  (
+    "Pexel",
+    "https://www.pexels.com/fr-fr/",
+    "Images and Videos",
+    "Assets, Images, Videos",
+    "Images et vidéos libre de droit/ banque d'images et photos gratuite"
+  ),
+  (
+    "Freepik",
+    "https://fr.freepik.com/",
+    "Images and Videos",
+    "Algorithms, Javascript",
+    "Interface web ludique pour vous aider à comprendre quelques bases JS/Algo."
+  ),
+  (
+    "Logoipsum",
+    "https://logoipsum.com/",
+    "practice",
+    "Mockups, UX/UI",
+    "faire un mockup de logo"
+  ),
+  (
+    "React GG",
+    "https://react.gg/ ",
+    "Overview",
+    "React",
+    "Un site interactif qui présente brièvement React, d'une manière intéressante"
+  );
