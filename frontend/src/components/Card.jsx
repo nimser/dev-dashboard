95460096aa6c85feea5ec081f5601d37be27c0d9
Nicolas Wormser <nicolas@codewithflair.org>
2023-06-13 00:42:40 +0200

    Fix AbortController issue, use custom fetcher instead

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,26 +3,34 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./card.module.css";
 
+// cache and reuse promises based on url (see https://devpress.csdn.net/react/62eb675520df032da732b24a.html)
+const createFetch = () => {
+  const fetchMap = {};
+
+  return (url, options) => {
+    if (!fetchMap[url]) {
+      const instance = axios.create({
+        baseURL: "https://api.embed.rocks",
+        headers: { "x-api-key": import.meta.env.VITE_OG_API_KEY },
+      });
+      fetchMap[url] = instance.get(url, options);
+    }
+
+    return fetchMap[url];
+  };
+};
+
 function Card({ resource }) {
   const [banner, setBanner] = useState(
     "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png"
   );
   const url = encodeURIComponent(resource.url);
-  const instance = axios.create({
-    baseURL: "https://api.embed.rocks",
-    headers: { "x-api-key": import.meta.env.VITE_OG_API_KEY },
-  });
+  const fetcher = createFetch();
   useEffect(() => {
-    const controller = new AbortController();
-    instance
-      .get(`/api?url=${url}`, { signal: controller.signal })
+    fetcher(`/api?url=${url}`)
       .then((response) => response.data.images?.[0])
       .then((image) => image && setBanner(image?.url))
       .catch((err) => console.error(err));
-
-    return () => {
-      controller.abort();
-    };
   }, []);
 
   return (
